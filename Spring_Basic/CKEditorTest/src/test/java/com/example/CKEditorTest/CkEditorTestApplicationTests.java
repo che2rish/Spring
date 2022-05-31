4f0842835df6837487cf1a51f173596d0fea572b
che2rish <88181171+che2rish@users.noreply.github.com>
2022-05-31 14:57:48 +0900

    SpringStudy

---
+++ Spring_Basic/CKEditorTest/src/test/java/com/example/CKEditorTest/CkEditorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CKEditorTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkEditorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
